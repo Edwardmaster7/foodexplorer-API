1509a553b8dde3545994931c8036fe95f921fc69
Eduardo Oliveira Batista <eduardoobatista2002@hotmail.com>
2024-10-16 16:05:10 -0300

    modified:   knexfile.js 	modified:   src/database/knex/index.js

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,17 @@ module.exports = {
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
     }
+  },
+
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
+    }
   }
 };
 
